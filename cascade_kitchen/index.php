e98c0bb9061133ac1cdf46858922c03466af5f5f
MaingiKiio <116945692+MaingiKii0@users.noreply.github.com>
2023-10-16 07:26:56 +0300

    Add files via upload
    Cascade kitchen app

---
+++ cascade_kitchen/index.php
@@ -0,0 +1,443 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
+
+    <!-- fOOD sEARCH Section Starts Here -->
+    <section class="food-search text-center">
+        <div class="container">
+            
+            <form action="food-search.html" method="POST">
+                <input type="search" name="search" placeholder="Search for Food.." required>
+                <input type="submit" name="submit" value="Search" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+    <!-- fOOD sEARCH Section Ends Here -->
+
+    <!-- CAtegories Section Starts Here -->
+    <section class="categories">
+        <div class="container">
+            <h2 class="text-center">Explore Foods</h2>
+
+            <a href="category-foods.html">
+            <div class="box-3 float-container">
+                <img src="../images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Pizza</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="../images/burger.jpg" alt="Burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Burger</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="../images/momo.jpg" alt="Momo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Momo</h3>
+            </div>
+            </a>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Categories Section Ends Here -->
+
+    <!-- fOOD menu Section Starts Here -->
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Hot Beverages</h2>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>African Mixed Tea</h4>
+                    <p class="food-price">sh.100</p>
+                    <p class="food-detail">
+                        Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Masala Tea</h4>
+                    <p class="food-price">sh.100</p>
+                    <p class="food-detail">
+                    Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Cascade Signature Natural Poridge</h4>
+                    <p class="food-price">sh.150</p>
+                    <p class="food-detail">
+                    Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Cascade Farm Fresh</h4>
+                    <p class="food-price">sh.100</p>
+                    <p class="food-detail">
+                    Glass of Milk
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <h2 class="text-center">Snacks</h2>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Arrow Roots</h4>
+                    <p class="food-price">sh.100</p>
+                    <p class="food-detail">
+                    Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Sweet Potatoes</h4>
+                    <p class="food-price">sh.80</p>
+                    <p class="food-detail">
+                    Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Cassava</h4>
+                    <p class="food-price">sh.80</p>
+                    <p class="food-detail">
+                    Served and delivered on time.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>African Plate</h4>
+                    <p class="food-price">sh.150</p>
+                    <p class="food-detail">
+                    Arrow Root, Sweet Potatoes, cassava
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Andazi</h4>
+                    <p class="food-price">sh.40</p>
+                    <p class="food-detail">
+                    
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Beef Samosa</h4>
+                    <p class="food-price">sh.70</p>
+                    <p class="food-detail">
+                    
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Beef Sausage</h4>
+                    <p class="food-price">sh.70</p>
+                    <p class="food-detail">
+                    
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Chapati</h4>
+                    <p class="food-price">sh.60</p>
+                    <p class="food-detail">
+                    
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>African Mix</h4>
+                    <p class="food-price">sh.250</p>
+                    <p class="food-detail">
+                    (Boiled/Spanish Egg, Nduma/Ngwacii/Cassava, Avocado/Stir fry vegetables/Carrot Peas) | Choose 3
+                    <br>
+                    <i>Served with Salad</i>
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Swahili Mix</h4>
+                    <p class="food-price">sh.250</p>
+                    <p class="food-detail">
+                    (Mbaazi in coconut/cowpeas curry, Mahamri/Matoke, fried Mchicha/Carrot Peas) | Choose 3
+                    <br>
+                    <i>Served with Salad</i>
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Cold Starter</h4>
+                    <p class="food-price">sh.100</p>
+                    <p class="food-detail">
+                    Mixed Vegetable Salad
+                    <br>
+                    <i>Kachumbari</i>
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Hot Starter</h4>
+                    <p class="food-price">sh.200</p>
+                    <p class="food-detail">
+                    Chicken Soup with Pieces of Chicken | Bone Soup with pieces of Beef
+                    <br>
+                    <i>Served with Crusty Rolls</i>
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-burger.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Nice Burger</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce, Chicken, and organice vegetables.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Food Title</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce, Chicken, and organice vegetables.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Food Title</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce, Chicken, and organice vegetables.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="../images/menu-momo.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Chicken Steam Momo</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce, Chicken, and organice vegetables.
+                    </p>
+                    <br>
+
+                    <a href="contact.php" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+
+
+            <div class="clearfix"></div>
+
+            
+
+        </div>
+
+        <p class="text-center">
+            <a href="#">See All Foods</a>
+        </p>
+    </section>
+    <!-- fOOD Menu Section Ends Here -->
+
+    <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+
+    <?php include('partials/footer.php'); ?>
---
+++ cascade_kitchen/partials/footer.php
@@ -0,0 +1,9 @@
+<section class="footer">
+        <div class="container text-center">
+            <p>Cascade Kitchen &copy;2023</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
